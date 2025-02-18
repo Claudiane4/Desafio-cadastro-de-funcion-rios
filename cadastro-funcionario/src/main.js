810f721eab70cd2e137f927cdf2a40b41b6623f5
Claudiane4 <claudiane.dias1332@gmail.com>
2025-02-17 20:57:08 -0400

    reformulado css e adicionado um script teste, apenas para fins de teste

--- cadastro-funcionario/src/main.js
+++ cadastro-funcionario/src/main.js
@@ -1,25 +1,10 @@
-const cpfinput = document.querySelector('#CPF')
+const input = document.querySelector('#cpf')
 
-input.addEventListener('keypress', () =>{
-    let inputlength = cpfinput.ValueMax.length
-
-    console.log(inputlength);
+input.addEventListener('#cpf', () => {
+    let inputlength = input.value.length
         if(inputlength === 3 || inputlength === 7){
-        cpfinput.value += '.';
-        }  
-        else if(inputlength === 11){
-        cpfinput.value += "-";
-    }
-})
-
-const rginput = document.querySelector('#RG')
-
-    input.addEventListener('keypress', () =>{
-        let inputlength = rginput.ValueMax.length
-
-        console.log(inputlength);
-        if(inputlength === 8){
-        input.value += '-';
-    }
-    
+        input.value += '.';
+        }else if(inputlength === 11){
+            input.value += "-";
+        }
 })
